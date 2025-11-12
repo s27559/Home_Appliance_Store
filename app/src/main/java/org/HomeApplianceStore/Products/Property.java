cba60a4090e972fa4a131657f30fb268d3f306c0
maxkzb <s31766@pjwstk.edu.pl>
2025-11-12 23:06:32 +0100

    added constructor for Property

--- app/src/main/java/org/HomeApplianceStore/Products/Property.java
+++ app/src/main/java/org/HomeApplianceStore/Products/Property.java
@@ -1,9 +1,21 @@
 package org.HomeApplianceStore.Products;
 
+import java.util.ArrayList;
+
 public class Property {
+
+        private static ArrayList<Property> properties = new ArrayList<Property>();
 
         private String typeName;
         private String value;
+
+        public Property(String typeName, String value) {
+            this.typeName = typeName;
+            this.value = value;
+
+            properties.add(this);
+        }
+
         public String getTypeName() {
                 return typeName;
         }
