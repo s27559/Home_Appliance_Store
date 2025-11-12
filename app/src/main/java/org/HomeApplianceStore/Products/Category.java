cabed15acf6ac5b73dbd37b9c20b3e6a882a984b
maxkzb <s31766@pjwstk.edu.pl>
2025-11-12 22:35:33 +0100

    Added ArrayList for categories

--- app/src/main/java/org/HomeApplianceStore/Products/Category.java
+++ app/src/main/java/org/HomeApplianceStore/Products/Category.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Category {
 
+        private static ArrayList<Category> categories = new ArrayList<>();
+
         private String name;
         private ArrayList<Property> properties;
         public String getName() {
