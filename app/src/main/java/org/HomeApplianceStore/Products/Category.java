0334a9d781ed0bd03f63e8d9e318215a6814fb60
maxkzb <s31766@pjwstk.edu.pl>
2025-11-12 22:50:27 +0100

    added categories array list

--- app/src/main/java/org/HomeApplianceStore/Products/Category.java
+++ app/src/main/java/org/HomeApplianceStore/Products/Category.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Category {
 
+        private static ArrayList<Category> categories = new ArrayList<Category>();
+
         private String name;
         private ArrayList<Property> properties;
         public String getName() {
