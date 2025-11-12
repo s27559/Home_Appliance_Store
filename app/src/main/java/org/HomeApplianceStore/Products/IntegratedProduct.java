3cad3012581423d554fc956521613d2991884b72
maxkzb <s31766@pjwstk.edu.pl>
2025-11-12 23:03:36 +0100

    added constructor for IntegratedProduct

--- app/src/main/java/org/HomeApplianceStore/Products/IntegratedProduct.java
+++ app/src/main/java/org/HomeApplianceStore/Products/IntegratedProduct.java
@@ -1,11 +1,22 @@
 package org.HomeApplianceStore.Products;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class IntegratedProduct {
 
+        private static ArrayList<IntegratedProduct> integratedProducts = new ArrayList<IntegratedProduct>();
+
         private BigDecimal integrationCost;
         private boolean mustBeDone;
+
+        public IntegratedProduct(String name, BigDecimal integrationCost, boolean mustBeDone) {
+            this.integrationCost = integrationCost;
+            this.mustBeDone = mustBeDone;
+
+            integratedProducts.add(this);
+        }
+
         public BigDecimal getIntegrationCost() {
                 return integrationCost;
         }
