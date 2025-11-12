3c31cd8a8077c0571cdbc0249d9734d09563e4cb
maxkzb <s31766@pjwstk.edu.pl>
2025-11-12 22:45:22 +0100

    added feestandingProducts array list

--- app/src/main/java/org/HomeApplianceStore/Products/FreestandingProduct.java
+++ app/src/main/java/org/HomeApplianceStore/Products/FreestandingProduct.java
@@ -1,8 +1,11 @@
 package org.HomeApplianceStore.Products;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class FreestandingProduct {
+
+        private static ArrayList<FreestandingProduct> freestandingProducts =new ArrayList<FreestandingProduct>();
 
         private BigDecimal moveCost;
 
