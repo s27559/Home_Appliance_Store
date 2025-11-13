03436625e7f13d036aad1c0367a2965f7b6c2d9b
s27559 <s27559@pjwstk.edu.pl>
2025-11-13 01:16:03 +0100

    added extent and dereived methods for Actors package

--- app/src/main/java/org/HomeApplianceStore/Actors/CustomerCompany.java
+++ app/src/main/java/org/HomeApplianceStore/Actors/CustomerCompany.java
@@ -1,11 +1,37 @@
 package org.HomeApplianceStore.Actors;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CustomerCompany extends Customer {
+import org.HomeApplianceStore.Extent;
+
+public class CustomerCompany extends Customer implements Extent{
+        private static ArrayList<CustomerCompany> customerCompanies = new ArrayList<>();
 
         private static BigDecimal bulkOrderDiscount;
         private Company company;
+
+        public CustomerCompany() {
+                super();
+                addCustomerCompany(this);
+        }
+
+        public static List<CustomerCompany> getCustomerCompanies() {
+                return Extent.getImmutableClassList(customerCompanies);
+        }
+
+        public static void loadCustomerCompanies(){
+                customerCompanies = Extent.loadClassList("./org/HomeApplianceStore/Actors/CustomerCompany.ser");
+        }
+
+        public static void saveCustomerCompanies(){
+                Extent.saveClassList("./org/HomeApplianceStore/Actors/CustomerCompany.ser", customerCompanies);
+        }
+
+        private static void addCustomerCompany(CustomerCompany customerCompany) {
+                customerCompanies.add(customerCompany);
+        }
 
         public Company getCompany() {
                 return company;
--- app/src/main/java/org/HomeApplianceStore/Actors/Employee.java
+++ app/src/main/java/org/HomeApplianceStore/Actors/Employee.java
@@ -1,8 +1,13 @@
 package org.HomeApplianceStore.Actors;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Employee {
+import org.HomeApplianceStore.Extent;
+
+public class Employee implements Extent{
+        private static ArrayList<Employee> employees = new ArrayList<>();
 
         private BigDecimal bonusPay;
         private long sickDays;
@@ -10,8 +15,34 @@ public class Employee {
         private long unpaidLeaveDays;
         // leaveDays
         // fullPay
+        
         private Person person;
 
+        public Employee(BigDecimal bonusPay, long sickDays, long paidLeaveDays, long unpaidLeaveDays) {
+                this.bonusPay = bonusPay;
+                this.sickDays = sickDays;
+                this.paidLeaveDays = paidLeaveDays;
+                this.unpaidLeaveDays = unpaidLeaveDays;
+
+                addEmployee(this);
+        }
+
+        public static List<Employee> getEmployees() {
+                return Extent.getImmutableClassList(employees);
+        }
+
+        public static void loadEmployees(){
+                employees = Extent.loadClassList("./org/HomeApplianceStore/Actors/Employee.ser");
+        }
+
+        public static void saveEmployees(){
+                Extent.saveClassList("./org/HomeApplianceStore/Actors/Employee.ser", employees);
+        }
+
+        private static void addEmployee(Employee employee) {
+                employees.add(employee);
+        }
+        
         public Person getPerson() {
                 return person;
         }
@@ -41,5 +72,13 @@ public class Employee {
         }
         public void setUnpaidLeaveDays(long unpaidLeaveDays) {
                 this.unpaidLeaveDays = unpaidLeaveDays;
+        }
+
+        public BigDecimal getFullPay(){
+                return new BigDecimal(0);
+        }
+
+        public long getLeaveDays(){
+                return paidLeaveDays + unpaidLeaveDays;
         }
 }
