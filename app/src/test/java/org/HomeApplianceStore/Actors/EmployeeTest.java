41c0823af98a014b8ff26114ad2fe3d2b0bb1c60
s27559 <s27559@pjwstk.edu.pl>
2025-11-19 17:15:25 +0100

    fixed Employee constructor test

--- app/src/test/java/org/HomeApplianceStore/Actors/EmployeeTest.java
+++ app/src/test/java/org/HomeApplianceStore/Actors/EmployeeTest.java
@@ -15,11 +15,13 @@ public class EmployeeTest {
                 long sickDays = 10;
                 long paidLeaveDays = 20;
                 long unpaidLeaveDays = 30;
-                Employee employee = new Employee(bonusPay, sickDays, paidLeaveDays, unpaidLeaveDays);
+                EmpRole role = EmpRole.CLERK;
+                Employee employee = new Employee(bonusPay, sickDays, paidLeaveDays, unpaidLeaveDays, role);
                 assertTrue(bonusPay.equals(employee.getBonusPay()));
                 assertTrue(sickDays == employee.getSickDays());
                 assertTrue(paidLeaveDays == employee.getPaidLeaveDays());
                 assertTrue(unpaidLeaveDays == employee.getUnpaidLeaveDays());
+                assertTrue(role == employee.getRole());
                 assertTrue(Employee.getEmployees().contains(employee));
         }
 }
